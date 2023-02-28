0a0d972e0ba66395e738f8082ec2d7b48ef9d456
muthupriyaesakkiappan <122095904+muthupriyaesakkiappan@users.noreply.github.com>
2023-02-28 14:11:01 +0530

    Add files via upload

---
+++ Cylinder.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Cylinder{
+public static void main(String[] args)
+{
+Scanner input=new Scanner(System.in);
+System.out.println("Enter first value");
+int r= input.nextInt();
+System.out.println("Enter first value");
+int h= input.nextInt();
+int volume=22*r*r*h/7;
+System.out.println("volume is:" + volume);
+}
+}
+
+
+
