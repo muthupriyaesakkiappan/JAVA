2bb5ee213bf2365645b0a6f144f1f2b3558c07d4
muthupriyaesakkiappan <122095904+muthupriyaesakkiappan@users.noreply.github.com>
2023-03-03 23:13:18 +0530

    Add files via upload

---
+++ Arithmetic1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Arithmetic1{
+public static void main(String[] args){
+Scanner in=new Scanner(System.in);
+
+System.out.println("Enter a value:");
+int a=in.nextInt();
+
+System.out.println("Enter b value:");
+int b=in.nextInt();
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println("Addition:" + a+b);
+System.out.println("Subtraction" + a-b);
+}
+}
+
+
