3d614222c6e8718b6b2c489067306f5b298a9984
muthupriyaesakkiappan <122095904+muthupriyaesakkiappan@users.noreply.github.com>
2023-03-03 23:15:14 +0530

    Add files via upload

---
+++ Arithmetic ex.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Arithmeticex{
+public static void main(String[] args){
+int a=3;
+int b=4;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+System.out.println(a%b);
+}
+}
+
