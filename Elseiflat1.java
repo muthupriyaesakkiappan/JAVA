892155298c0a41ca14a2e90e96a8a97e9b75644f
muthupriyaesakkiappan <122095904+muthupriyaesakkiappan@users.noreply.github.com>
2023-03-03 22:54:44 +0530

    Add files via upload

---
+++ Elseiflat1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Elseiflat1{
+public static void main(String[] args){
+Scanner input =new Scanner(System.in);
+System.out.println("Enter your number");
+int a=input.nextInt();
+int b= 123;
+
+if(a==b){
+System.out.println("Condition 1 satisfied");
+}
+else if(a!=1234){
+System.out.println("Condition 2 satisfited");
+}
+else{
+System.out.println("Both are not satisfied");
+}
+}
+}
+
---
+++ Ifexample4.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Ifexample4{
+public static void main(String[] args)
+{
+int age=20;
+if(age>18){
+System.out.println("the person is eligible for vote");
+}
+}
+}
